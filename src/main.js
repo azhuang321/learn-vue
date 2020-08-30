640aa606c091c90de101d88e45e0f9852aaa084b
azhuang <azhuang321@gmail.com>
2020-08-30 09:39:14 +0800

    接口同一拦截和错误处理和请求代理

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import Vue from 'vue';
-import router from './router';
+import router from 'router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue';
+
+// 根据前端跨域方式做调整
+axios.defaults.baseURL = '/api';
+
+//返回值接口拦截
+axios.interceptors.response.use(function (response) {
+  let res = response.data
+  if ( res.status == 0 ) {
+      return res.data
+  }else if ( res.status == 10 ) { //未登录
+      window.location.href('/#/login')
+  }else {
+    alert(res.msg)
+  }
+})
+
+
+Vue.use(VueAxios,axios);
+
 
 Vue.config.productionTip = false
 
