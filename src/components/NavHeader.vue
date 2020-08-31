23b623013d4c329a44ebfac6a8770f6709a737b4
azhuang <azhuang321@gmail.com>
2020-08-31 10:48:18 +0800

    5-2-1:实现logo的动画

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -16,6 +16,16 @@
         </div>
       </div>
     </div>
+    <div class="nav-header">
+      <div class="container">
+        <div class="header-logo">
+          <a href="/#/index"></a>
+        </div>
+      </div>
+    </div>
+
+
+
   </div>
 </template>
 
@@ -26,6 +36,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/base";
+
  .header{
    .nav-topbar{
      height: 39px;
@@ -56,6 +68,47 @@ export default {
          }
        }
 
+     }
+   }
+   .nav-header{
+     .container {
+       position:relative;
+       height: 112px;
+       display: flex;
+       justify-content: space-between;
+       align-items: center;
+       .header-logo {
+         display: inline-block;
+         width: 55px;
+         height: 55px;
+         background-color: #FF6600;
+         a {
+           display: inline-block;
+           width: 110px;
+           height: 55px;
+           &:before {
+             content: ' ';
+             display: inline-block;
+             width: 55px;
+             height: 55px;
+             background: url("/imgs/mi-logo.png") no-repeat center;
+             background-size: 55px;
+             transition: margin 0.2s;
+           }
+           &:after {
+             content: ' ';
+             display: inline-block;
+             width: 55px;
+             height: 55px;
+             background: url("/imgs/mi-home.png") no-repeat center;
+             background-size: 55px;
+           }
+           &:hover:before {
+             margin-left: -55px;
+             transition: margin 0.2s;
+           }
+         }
+       }
      }
    }
  }
