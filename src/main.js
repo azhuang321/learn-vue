8080ddef660b1a1e30d4014f0f2e46c1af6d8c63
azhuang <azhuang321@gmail.com>
2020-08-31 21:07:59 +0800

    6-1:登录页功能

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueLazyload from "vue-lazyload";
+import VueCookie from 'vue-cookie';
 import App from './App.vue';
 
 // 根据前端跨域方式做调整
@@ -23,18 +24,23 @@ if(mock){
 
 //返回值接口拦截
 axios.interceptors.response.use(function (response) {
-  let res = response.data
-  if ( res.status == 0 ) {
-      return res.data
-  }else if ( res.status == 10 ) { //未登录
-      window.location.href('/#/login')
-  }else {
-    alert(res.msg)
-  }
+    let res = response.data;
+    let path = location.hash;
+    if ( res.status == 0 ) {
+        return res.data
+    }else if ( res.status == 10 ) { //未登录
+        if ( path != '#/index') {
+            window.location.href('/#/login')
+        }
+    }else {
+        alert(res.msg);
+        return Promise.reject(res);
+    }
 })
 
 
 Vue.use(VueAxios,axios);
+Vue.use(VueCookie);
 Vue.use(VueLazyload,{
     loading:'/imgs/loading-svg/loading-bars.svg'
 })
--- src/router.js
+++ src/router.js
@@ -14,6 +14,7 @@ import OrderList from "./pages/orderList";
 import OrderPay from "./pages/orderPay";
 import AliPay from "./pages/alipay"
 
+import Login from './pages/login'
 
 Vue.use(Router);
 
@@ -46,6 +47,11 @@ export default new Router({
             path: '/cart'
             , name: 'cart'
             , component: Cart
+        }
+        , {
+            path: '/login'
+            , name: 'login'
+            , component: Login
         }
         , {
             path: '/order'
