705589537d23c9f3622f388241f2b8594ce22d4f
azhuang <azhuang321@gmail.com>
2020-09-05 09:50:04 +0800

    12-1:调整订单页面

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <order-header></order-header>
+    <order-header :title="title">
+      <template v-slot:tip>
+        <span>{{ tip }}</span>
+      </template>
+    </order-header>
     <router-view></router-view>
+    <service-bar></service-bar>
     <nav-footer></nav-footer>
   </div>
 
@@ -9,10 +14,30 @@
 
 <script>
 import OrderHeader from "@/components/OrderHeader";
+import ServiceBar from "@/components/ServiceBar";
 import NavFooter from "@/components/NavFooter";
 export default {
   name: "order",
-  components: {NavFooter, OrderHeader}
+  components: {NavFooter, OrderHeader,ServiceBar}
+  ,data(){
+    return{
+      title:''
+      ,tip:''
+    }
+  }
+  ,mounted() {
+    let path = this.$route.path;
+    if ( path == '/order/confirm' ) {
+      this.title = '订单确认';
+      this.tip = '请填写收货地址';
+    }else if( path == '/order/list' ) {
+      this.title = '订单列表';
+      this.tip = '天干物燥，谨防诈骗';
+    }else if( path == '/order/pay' ) {
+      this.title = '订单支付';
+      this.tip = '天干物燥，谨防诈骗';
+    }
+  }
 }
 </script>
 
